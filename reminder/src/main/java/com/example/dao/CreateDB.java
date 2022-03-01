8bbe5c4d6173418659a8d86e50ffecc61f53ff90
NicoStollFHWS <nicolas.stoll@student.fhws.de>
2022-03-01 17:08:07 +0100

    kleine Änderungen in der DB - kleine Fehler in der Syntax behoben - Auslagerung in mehrere Methoden

--- reminder/src/main/java/com/example/dao/CreateDB.java
+++ reminder/src/main/java/com/example/dao/CreateDB.java
@@ -7,76 +7,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import org.apache.derby.jdbc.EmbeddedDriver;
 
 public class CreateDB {
 
-    public static void main(String[] args) {
-        CreateDB db = new CreateDB();
-        db.testDerby();
+    private Connection conn = null;
+
+    public CreateDB() {
+        createDerbyDB();
     }
-    public void testDerby() {
-        Connection conn = null;
+    public void createDerbyDB() {
         PreparedStatement pstmt;
         Statement stmt;
         ResultSet rs;
 
         String createFachverfahrenSQL =
-                "CREATE TABLE IF NOT EXISTS fachverfahren ( "
+                "CREATE TABLE fachverfahren ( "
                 + "id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                 + "name VARCHAR(255) NOT NULL, "
-                + "CONSTRAINT primary_key PRIMARY KEY(id)); ";
-
+                + "CONSTRAINT primary_key_fachverfahren PRIMARY KEY(id))";
 
         String createITVerfahrenSQL =
-                "CREATE TABLE IF NOT EXISTS itverfahren("
+                "CREATE TABLE itverfahren ( "
                 + "id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "
                 + "name VARCHAR(255) NOT NULL, "
                 + "itKuerzel VARCHAR(255) NOT NULL, "
                 + "team VARCHAR(255) NOT NULL, "
                 + "ansprechpartner VARCHAR(255) NOT NULL, "
-                + "fachverfahrenId INTEGER NOT NULL"
-                + "CONSTRAINT primary_key PRIMARY KEY(id), "
-                + "CONSTRAINT fachverfahren_id_ref FOREIGN KEY (fachverfahrenId) REFERENCES fachverfahren(id));";
+                + "fachverfahrenId INTEGER NOT NULL, "
+                + "CONSTRAINT primary_key_itverfahren PRIMARY KEY(id), "
+                + "CONSTRAINT fachverfahren_id_ref FOREIGN KEY (fachverfahrenId) REFERENCES fachverfahren(id))";
 
         try {
             Driver derbyEmbeddedDriver = new EmbeddedDriver();
             DriverManager.registerDriver(derbyEmbeddedDriver);
-            conn = DriverManager.getConnection
+            this.conn = DriverManager.getConnection
                     ("jdbc:derby:verfahrensliste;create=true");
-            conn.setAutoCommit(false);
+            this.conn.setAutoCommit(false);
             stmt = conn.createStatement();
             stmt.execute(createFachverfahrenSQL);
             stmt.execute(createITVerfahrenSQL);
 
-            /*
+            this.conn.commit();
 
-            pstmt = conn.prepareStatement("insert into person (name,email,phone) values(?,?,?)");
-            pstmt.setString(1, "Hagar the Horrible");
-            pstmt.setString(2, "hagar@somewhere.com");
-            pstmt.setString(3, "1234567890");
-            pstmt.executeUpdate();
-
-
-
-            rs = stmt.executeQuery("select * from person");
-            while (rs.next()) {
-                System.out.printf("%d %s %s %sn",
-                        rs.getInt(1), rs.getString(2),
-                        rs.getString(3), rs.getString(4));
-            }
-
-            stmt.execute("drop table person");
-
-            conn.commit();
-
-             */
 
         } catch (SQLException ex) {
             System.out.println("in connection" + ex);
         }
+    }
 
+    public void shutDown() {
         try {
             DriverManager.getConnection
                     ("jdbc:derby:;shutdown=true");
@@ -89,5 +69,42 @@ public class CreateDB {
                 System.err.println(ex.getMessage());
             }
         }
+    }
+
+    public void dummyData() {
+        PreparedStatement pstmt;
+        Statement stmt;
+        ResultSet rs;
+
+        try {
+            stmt = this.conn.createStatement();
+            pstmt = this.conn.prepareStatement("insert into fachverfahren (name) values(?)");
+            pstmt.setString(1, "F.001 Anspruchs und Antwartschaftüberführungsgesetz");
+            pstmt.executeUpdate();
+
+            pstmt = this.conn.prepareStatement("INSERT INTO itverfahren (name, itKuerzel, team, ansprechpartner, fachverfahrenId) values (?,?,?,?,?)");
+            pstmt.setString(1, "C.001.01 Anspruchs- und Anwartschaftsüberführungsgesetz");
+            pstmt.setString(2, "aaug");
+            pstmt.setString(3, "0551-11");
+            pstmt.setString(4, "Wolfgang Möckl");
+            pstmt.setInt(5, 1);
+            pstmt.executeUpdate();
+
+            rs = stmt.executeQuery("select * from itverfahren");
+            while (rs.next()) {
+                System.out.printf("%d %s %s %s %s %s\n",
+                        rs.getInt(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4),
+                        rs.getString(5), rs.getString(6));
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getErrorCode());
+        }
+
+    }
+
+    public Connection getConn() {
+        return conn;
     }
 }
--- reminder/src/main/java/com/example/dao/Repository.java
+++ reminder/src/main/java/com/example/dao/Repository.java
@@ -1,4 +1,12 @@
 package com.example.dao;
 
+import java.sql.Connection;
+
 public class Repository {
+
+    Connection conn;
+
+    public Repository() {
+        this.conn = new CreateDB().getConn();
+    }
 }
