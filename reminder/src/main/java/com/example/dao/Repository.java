9b9d2eaca3d93566327353cad8a07159f9a93449
Tim <tim.noeth@googlemail.com>
2022-03-02 15:48:35 +0100

    Repository Funktionen hinzugefügt

--- reminder/src/main/java/com/example/dao/Repository.java
+++ reminder/src/main/java/com/example/dao/Repository.java
@@ -9,4 +9,103 @@ public class Repository {
     public Repository() {
         this.conn = new CreateDB().getConn();
     }
+
+    /**
+     * Gibt die komplette Verfahrensliste aus
+     * @return Verfahrensliste
+     */
+    public Verfahrensliste getVerfahrensliste() {
+
+        Verfahrensliste verfahrensliste = new Verfahrensliste();
+        verfahrensliste.setFachverfahren(getAllFachverfahren());
+        return verfahrensliste;
+    }
+
+    /**
+     * Gibt alle Fachverfahren mit dazugehörigen ItVerfahren aus
+     * @return List<Fachverfahren>
+     */
+    public List<Fachverfahren> getAllFachverfahren() {
+        List<Fachverfahren> fachverfahren = new ArrayList<>();
+        String getSQL = "SELECT * FROM fachverfahren";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(getSQL);
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+
+            while(rs.next()) {
+                Fachverfahren f = new Fachverfahren(rs.getInt(1), rs.getString(2));
+                f.setItVerfahren(getAllItVerfahren(f.getId()));
+                fachverfahren.add(f);
+            }
+        } catch (SQLException e) {
+            System.err.println("Methode getAllFachverfahren hat nicht funktioniert: \n" + e.getErrorCode());
+        }
+        return fachverfahren;
+    }
+
+
+    /**
+     * Gibt alle ItVerfahren zu einem bestimmten Fachverfahren aus
+     * @return List<ITVerfahren>
+     */
+    public List<ITVerfahren> getAllItVerfahren(int fachverfahrenId) {
+        String getSQL = "SELECT * FROM fachverfahren f " +
+                "JOIN itverfahren i on f.id = i.fachverfahrenId " +
+                "WHERE f.id = " + fachverfahrenId;
+
+        List<ITVerfahren> itVerfahren = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(getSQL);
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String itKuerzel = rs.getString(3);
+                String team = rs.getString(4);
+                String ansprechpartner = rs.getString(5);
+
+                itVerfahren.add(new ITVerfahren(id, name, itKuerzel, team, ansprechpartner));
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Methode getAllItVerfahren hat nicht funktioniert:\n" + e.getMessage());
+        }
+
+        return itVerfahren;
+
+    }
+
+    public ITVerfahren getItVerfahren(int fachverfahrensId, int itVerfahrensId) {
+        String SQL =
+                "SELECT * "
+                + "FROM itverfahren"
+                + "WHERE id =" + itVerfahrensId + " AND fachverfahrenId = " + fachverfahrensId + " ;";
+        ITVerfahren itVerfahren = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(SQL);
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String itKuerzel = rs.getString(3);
+                String team = rs.getString(4);
+                String ansprechpartner = rs.getString(5);
+
+                itVerfahren = new ITVerfahren(id, name, itKuerzel, team, ansprechpartner);
+            }
+        } catch (SQLException e) {
+            System.err.println("Methode getItVerfharen SQLAbfrage hat nicht funktioniert:\n" + e.getErrorCode());
+        }
+
+        return itVerfahren;
+    }
+
+
 }
